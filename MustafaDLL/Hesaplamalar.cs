7913620f86cda2a4257716e972ccda3491669d21
Mustafa BÜKÜLMEZ <mustafabukulmez3446@gmail.com>
2021-05-19 20:24:42 +0300

    proje tamamlandı

---
+++ MustafaDLL/Hesaplamalar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MustafaDLL{
+    public class Hesaplamalar
+    {
+        public static double Toplama(double i1, double i2, double i3)
+        {
+            return i1 + i2 + i3;
+        }
+
+    }
+}
---
+++ Proje/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+using MustafaDLL;
+namespace Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Hesaplamalar.Toplama(5, 6, 6).ToString());
+        }
+    }
+}
